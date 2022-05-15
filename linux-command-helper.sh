ea31ba01234535bb29af92235daa2759cc825319
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-15 10:18:46 +0900

    [2019203087] 기존에 작성한 코드를 run 함수에서 실행하도록 수정
    - 앞으로 진행되는 모든 코드는 run 내부에서 실행
    - 함수 작성 시 구글에서 권장하는 방식으로 주석 작성

--- linux-command-helper.sh
+++ linux-command-helper.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-title=`cat resource/title.txt`
-echo "${title}"
+#######################################
+# 전체 스크립트를 실행합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function run() {
+    title=`cat resource/title.txt`
+    echo "${title}"
+}
+
+# 스크립트를 실행합니다.
+run
