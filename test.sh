966532ba724e29cc0733d0e8d7cc5021e989e7d6
lhc9 <104617387+lhc9@users.noreply.github.com>
2022-05-16 20:29:38 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,30 @@
+#！/bin/bash
+fileName=$1
+now=`date +%Y%y%d`
+dir=$(/data/$now)
+read  -p "Are you sure you want to delete this file or directory $1 ? yes|no :" input
+if [ $input == "yes" ] || [ $input == "y" ]; then
+
+     # Define DIR and mkdir DIR
+     
+     if [ ! -d $dir ]; then
+        mkdir /data/$now
+    fi
+    
+    # Rsync Synchronizes files and directories to be deleted
+    
+     rsync -aR $1/ /data/$now/$1/
+     rm -rf $1
+    
+elif [ $input == "no" ] || [ $input == "n" ]; then
+
+     # Exit with No
+     
+     exit 0
+else
+
+     # Prompt if you select another input character
+     
+     echo "Can only enter yes or no"
+     exit 0
+fi
