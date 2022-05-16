d95ab9e36acbe267cc31ab56bb81d13e18da31d2
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-16 22:38:59 +0900

    [2019203087] 전체 터미널에 표시된 텍스트를 지우는 기능 구현

--- linux-command-helper.sh
+++ linux-command-helper.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 #######################################
+# 전체 터미널에 표시된 텍스트를 비웁니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function clear_terminal() {
+     echo -en `clear`
+}
+
+#######################################
 # 전체 스크립트를 실행합니다.
 # Globals:
 #   None
@@ -10,6 +23,8 @@
 #   None
 #######################################
 function linux_command_helper() {
+    clear_terminal
+
     title=`cat resource/title.txt`
     echo "${title}"
 }
