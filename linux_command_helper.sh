518313df8f7712a1ff1d0801e8c54ca4715dc1b7
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-05-24 16:22:41 +0900

    [2019203087] 커서 이동 기능 추가

--- linux_command_helper.sh
+++ linux_command_helper.sh
@@ -4,6 +4,12 @@ function clear_terminal() {
     echo -n `clear`
 }
 
+function move_cursor() {
+     line=$1
+     col=$2
+     tput cup ${line} ${col}
+}
+
 function linux_command_helper() {
     echo "Hello World!"
 }
