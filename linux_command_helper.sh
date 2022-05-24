bdc413c372c7b388532c0fdb57cd645b9472fe13
DESKTOP-N6FOOG4\choij <choijione02@gmail.com>
2022-05-24 16:39:49 +0900

    [2019203087] 텍스트의 색상 변경 기능 추가
    - 텍스트의 문자열/배경의 색상 변경 기능 추가
    - 색상을 변경할 때, 문자열 혹은 배경 둘 중 하나만 설정할 수 있다.
    - 설정할 수 있는 색상은 8가지가 최대이다.

--- linux_command_helper.sh
+++ linux_command_helper.sh
@@ -20,8 +20,83 @@ function visible_cursor() {
     fi
 }
 
+function set_text_color() {
+    text_type=$1
+    color_type=$2
+
+    case ${color_type} in
+    $"black")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 0
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 0
+    fi
+    ;;
+
+    $"red")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 1
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 1
+    fi
+    ;;
+
+    $"green")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 2
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 2
+    fi
+    ;;
+
+    $"yellow")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 3
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 3
+    fi
+    ;;
+
+    $"blue")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 4
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 4
+    fi
+    ;;
+
+    $"magenta")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 5
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 5
+    fi
+    ;;
+
+    $"cyan")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 6
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 6
+    fi
+    ;;
+
+    $"white")
+    if [ "${text_type}" == "background" ]; then
+        tput setab 7
+    elif [ "${text_type}" == "string" ]; then
+        tput setaf 7
+    fi
+    ;;
+    *)
+    echo -n ""
+    ;;
+    esac
+}
+
 function linux_command_helper() {
     visible_cursor "off"
+    set_text_color "string" "yellow"
     echo "Hello World!"
     visible_cursor "on"
 }
