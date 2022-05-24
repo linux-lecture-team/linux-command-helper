0e25df3c4792bccf277b57a4dfd7e32d94efc8c3
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-24 21:55:28 +0900

    [2019203087] 주석문 추가 및 코드 정리
    - 참고 : Google Shell Script Guide
    - 전역 변수의 경우, 전부 스크립트 상단에 위치하도록 수정

--- linux_command_helper.sh
+++ linux_command_helper.sh
@@ -1,18 +1,144 @@
 #!/bin/bash
 
-##############################################################
-# 스크립트 공통 기능
-##############################################################
+# linux_command_helper의 메인 타이틀입니다.
+linux_command_helper_main_title=`cat resource/main_title.txt`
+
+# linux_command_helper의 메뉴얼입니다.
+linux_command_helper_manual=`cat resource/linux_command_helper.txt`
+
+# ls_helper의 메뉴얼입니다.
+ls_helper_manual=`cat resource/ls_helper.txt`
+
+# rm_helper의 메뉴얼입니다.
+rm_helper_manual=`cat resource/rm_helper.txt`
+
+# user_management_helper의 메뉴얼입니다.
+user_management_helper_manual=`cat resource/user_management_helper.txt`
+
+# go_to_github_repository의 메뉴얼입니다.
+go_to_github_repository_manual=`cat resource/go_to_github_repository.txt`
+
+# exit_linux_command_helper의 메뉴얼입니다.
+exit_linux_command_helper_manual=`cat resource/exit_linux_command_helper.txt`
+
+# 선택 메뉴 목록입니다.
+declare -a select_menu_list=(
+    "[01] linux command helper"
+    "[02] ls helper"
+    "[03] rm helper"
+    "[04] user management helper"
+    "[05] go to github repository"
+    "[06] exit linux command helper"
+)
+
+# 선택 메뉴 목록의 인덱스입니다.
+declare -i select_menu_index=0
+
+# 선택 메뉴 목록의 최소 인덱스입니다.
+declare -i select_menu_min_index=0
+
+# 선택 메뉴 목록의 최대 인덱스입니다.
+declare -i select_menu_max_index=5
+
+# 선택 메뉴 목록의 크기입니다.
+declare -i select_menu_list_size=6
+
+# 선택 메뉴의 터미널상 라인 위치입니다.
+declare -i select_menu_line=14
+
+# 선택 메뉴의 터미널상 열 위치입니다.
+declare -i select_menu_col=2
+
+# 실행 여부 목록입니다.
+declare -a select_ok_list=(
+    "[    OK    ]"
+    "[  CANCEL  ]"
+)
+
+# 실행 여부 목록의 인덱스입니다.
+declare -i select_ok_index=0
+
+# 실행 여부 목록의 최소 인덱스입니다.
+declare -i select_ok_min_index=0
+
+# 실행 여부 목록의 최대 인덱스입니다.
+declare -i select_ok_max_index=1
+
+# 실행 여부 목록의 크기입니다.
+declare -i select_ok_list_size=2
+
+# 실행 여부의 터미널 상 라인 위치입니다.
+declare -i select_ok_line=18
+
+# 실행 여부의 터미널 상 열 위치입니다.
+declare -i select_ok_col=40
+
+# 선택 메뉴의 실행 여부입니다.
+declare -i is_select_ok=0
+
+# 메뉴얼 목록입니다.
+declare -a manual_list=(
+    "${linux_command_helper_manual}"
+    "${ls_helper_manual}"
+    "$rm_helper_manual"
+    "${user_management_helper_manual}"
+    "${go_to_github_repository_manual}"
+    "${exit_linux_command_helper_manual}"
+)
+
+# 메뉴얼 목록의 크기입니다.
+declare -i manual_list_size=6
+
+# 메뉴얼의 터미널 상 라인 위치입니다.
+declare -i manual_line=21
+
+# 메뉴얼의 터미널 상 열 위치입니다.
+declare -i manual_col=0
+
+# 스크립트 루프의 실행 여부입니다.
+is_break_loop="no"
+
+
+#######################################
+# 터머널의 텍스트를 초기화합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function clear_terminal() {
     echo -n `clear`
 }
 
+
+#######################################
+# 터머널 커서의 위치를 변경합니다.
+# Globals:
+#   None
+# Arguments:
+#   $1 이동할 커서의 라인입니다.
+#   $2 이동할 커서의 열입니다.
+# Outputs:
+#   None
+#######################################
 function move_cursor() {
      line=$1
      col=$2
      tput cup ${line} ${col}
 }
 
+
+#######################################
+# 터머널 커서의 보이기/숨기기 설정을 수행합니다.
+# Globals:
+#   None
+# Arguments:
+#   $1 보이기/숨기기를 설정할 옵션입니다.
+# Outputs:
+#   None
+#######################################
 function visible_cursor() {
     option=$1
 
@@ -23,6 +149,17 @@ function visible_cursor() {
     fi
 }
 
+
+#######################################
+# 터머널의 텍스트 색상을 설정합니다.
+# Globals:
+#   None
+# Arguments:
+#   $1 터미널 텍스트의 타입입니다.
+#   $2 터미널 텍스트의 색상입니다.
+# Outputs:
+#   None
+#######################################
 function set_text_color() {
     text_type=$1
     color_type=$2
@@ -97,6 +234,16 @@ function set_text_color() {
     esac
 }
 
+
+#######################################
+# 방향키와 엔터키를 입력받습니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   입력받은 키의 문자열을 반환합니다.
+#######################################
 function input_enter_and_arrow_key() {
     escape_char=$(printf "\u1b")
     read -rsn1 key
@@ -116,36 +263,30 @@ function input_enter_and_arrow_key() {
 }
 
 
-##############################################################
-# linux command helper 내부 리소스
-##############################################################
-linux_command_helper_main_title=`cat resource/main_title.txt`
-
-
-##############################################################
-# select menu
-##############################################################
-declare -a select_menu_list=(
-    "[01] linux command helper"
-    "[02] ls helper"
-    "[03] rm helper"
-    "[04] user management helper"
-    "[05] go to github repository"
-    "[06] exit linux command helper"
-)
-
-declare -i select_menu_index=0
-declare -i select_menu_min_index=0
-declare -i select_menu_max_index=5
-declare -i select_menu_list_size=6
-declare -i select_menu_line=14
-declare -i select_menu_col=2
-
+#######################################
+# 터미널에 메인 타이틀을 표시합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function draw_main_title() {
     move_cursor 0 0
     echo "${linux_command_helper_main_title}"
 }
 
+
+#######################################
+# 터미널에 선택 메뉴를 표시합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function draw_select_menu() {
      for((index=0;index<${select_menu_list_size};index++))
      do
@@ -162,6 +303,16 @@ function draw_select_menu() {
      set_text_color "background" "black"
 }
 
+
+#######################################
+# 선택 메뉴의 인덱스를 감소시킵니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function up_select_menu_index() {
      select_menu_index=`expr ${select_menu_index} - 1`
 
@@ -170,6 +321,16 @@ function up_select_menu_index() {
      fi
 }
 
+
+#######################################
+# 선택 메뉴의 인덱스를 증가시킵니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function down_select_menu_index() {
      select_menu_index=`expr ${select_menu_index} + 1`
 
@@ -179,22 +340,15 @@ function down_select_menu_index() {
 }
 
 
-##############################################################
-# ok 기능
-##############################################################
-declare -a select_ok_list=(
-    "[    OK    ]"
-    "[  CANCEL  ]"
-)
-
-declare -i select_ok_index=0
-declare -i select_ok_min_index=0
-declare -i select_ok_max_index=1
-declare -i select_ok_list_size=2
-declare -i select_ok_line=18
-declare -i select_ok_col=40
-declare -i is_select_ok=0
-
+#######################################
+# 터미널에 실행 여부를 표시합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function draw_ok() {
 
     if [ ${is_select_ok} == 1 ]; then
@@ -226,6 +380,16 @@ function draw_ok() {
     fi
 }
 
+
+#######################################
+# 실행 여부 인덱스를 감소시킵니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function left_select_ok_index() {
     select_ok_index=`expr ${select_ok_index} - 1`
 
@@ -234,6 +398,16 @@ function left_select_ok_index() {
     fi
 }
 
+
+#######################################
+# 실행 여부 인덱스를 증가시킵니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function right_select_ok_index() {
     select_ok_index=`expr ${select_ok_index} + 1`
 
@@ -243,29 +417,15 @@ function right_select_ok_index() {
 }
 
 
-##############################################################
-# 메뉴얼
-##############################################################
-linux_command_helper_manual=`cat resource/linux_command_helper.txt`
-ls_helper_manual=`cat resource/ls_helper.txt`
-rm_helper_manual=`cat resource/rm_helper.txt`
-user_management_helper_manual=`cat resource/user_management_helper.txt`
-go_to_github_repository_manual=`cat resource/go_to_github_repository.txt`
-exit_linux_command_helper_manual=`cat resource/exit_linux_command_helper.txt`
-
-declare -a manual_list=(
-    "${linux_command_helper_manual}"
-    "${ls_helper_manual}"
-    "$rm_helper_manual"
-    "${user_management_helper_manual}"
-    "${go_to_github_repository_manual}"
-    "${exit_linux_command_helper_manual}"
-)
-
-declare -i manual_list_size=6
-declare -i manual_line=21
-declare -i manual_col=0
-
+#######################################
+# 터미널에 메뉴얼을 표시합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function draw_manual() {
     move_cursor ${manual_line} ${manual_col}
 
@@ -283,12 +443,15 @@ function draw_manual() {
 }
 
 
-
-##############################################################
-# 전체 루프 상태 업데이트
-##############################################################
-is_break_loop="no"
-
+#######################################
+# 스크립트 루프의 상태를 업데이트합니다.
+# Globals:
+#   None
+# Arguments:
+#   $1 키 값입니다.
+# Outputs:
+#   None
+#######################################
 function update_loop_state() {
     detect_input_key=$1
 
@@ -328,9 +491,15 @@ function update_loop_state() {
     fi
 }
 
-##############################################################
-# 사용자가 선택한 옵션 실행
-##############################################################
+#######################################
+# 사용자가 선택한 옵션을 실행합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function run_select_option() {
     case ${select_menu_index} in
     1)
@@ -356,9 +525,15 @@ function run_select_option() {
 }
 
 
-##############################################################
-# 전체 스크립트 실행 부분 시작
-##############################################################
+#######################################
+# linux_command_helper를 실행합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function linux_command_helper() {
     visible_cursor "off"
     set_text_color "string" "white"
@@ -394,4 +569,5 @@ function linux_command_helper() {
     run_select_option
 }
 
+# linux_command_helper를 실행합니다.
 linux_command_helper
