0da168c32d462c8cd742cbd7d7b34bdf9d8c576d
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-27 12:40:04 +0900

    [2019203087] ls_helper 기능 추가 구현
    - ls_helper 의 메뉴얼 추가

---
+++ source/ls_helper.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function clear_terminal() {
+    echo -n `clear`
+}
+
+function move_cursor() {
+     line=$1
+     col=$2
+     tput cup ${line} ${col}
+}
+
+declare -i input_cursor_line=10
+declare -i input_cursor_col=0
+
+function ls_helper() {
+    clear_terminal
+
+    ls_helper_manual=`cat resource/ls_helper_manual.txt`
+    echo "${ls_helper_manual}"
+
+    move_cursor ${input_cursor_line} ${input_cursor_col}
+    echo "ls 명령어를 수행할 절대 경로 혹은 상대 경로를 입력하세요..."
+    read -p "> " ls_target_path
+
+    move_cursor `expr ${input_cursor_line} + 2` ${input_cursor_col}
+    echo "ls 명령어의 옵션을 -를 제외하고 입력하세요..."
+    read -p "> " ls_target_option
+
+    cd ${ls_target_path}
+
+    if [ "${ls_target_option}" == "" ]; then
+        ls_execute_result=`ls`
+    else
+        ls_execute_result=`ls -${ls_target_option}`
+    fi
+    
+    echo "${ls_execute_result}"
+}
+
+ls_helper
+
