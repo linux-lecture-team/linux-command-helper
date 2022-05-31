e06f5f563d78145b8001c04a0aaedb14173bbebe
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-31 22:54:37 +0900

    [2019203087] 주석문 추가 및 메뉴얼 출력 부분 분리

--- source/ls_helper.sh
+++ source/ls_helper.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
+# 입력을 받는 커서의 라인입니다.
 declare -i input_cursor_line=10
+
+# 입력을 받는 커서의 열입니다.
 declare -i input_cursor_col=0
 
+# ls_helper의 옵션 메뉴얼입니다.
+ls_helper_manual=`cat resource/ls_helper_manual.txt`
+
+
+#######################################
+# 터미널에 ls_helper 메뉴얼을 표시합니다.
+# Globals:
+#   ls_helper_manual
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function draw_ls_helper_manual() {
+    echo "${ls_helper_manual}"
+}
+
+
+#######################################
+# ls_helper를 실행합니다.
+# Globals:
+#   None
+# Arguments:
+#   $1 텍스트 파일명
+# Outputs:
+#   None
+#######################################
 function ls_helper() {
+    # 시작 전에 터미널을 초기화합니다.
     clear_terminal
 
-    ls_helper_manual=`cat resource/ls_helper_manual.txt`
-    echo "${ls_helper_manual}"
 
+    # ls_helper 메뉴얼을 터미널에 표시합니다.
+    draw_ls_helper_manual
+    
+    
     move_cursor ${input_cursor_line} ${input_cursor_col}
     echo "ls 명령어를 수행할 절대 경로 혹은 상대 경로를 입력하세요..."
     read -p "> " ls_target_path
@@ -26,5 +59,5 @@ function ls_helper() {
     echo "${ls_execute_result}"
 }
 
+# ls_helper를 실행합니다.
 ls_helper
-
