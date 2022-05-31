6d774b035c658f2d1cb4f8d7ab7b1344ed22dda2
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-31 23:09:43 +0900

    [2019203087] rm_helper 주석문 추가 및 메뉴얼 출력 부분 분리

--- source/rm_helper.sh
+++ source/rm_helper.sh
@@ -1,25 +1,64 @@
 #!/bin/bash
 
+# 입력을 받는 커서의 라인입니다.
 declare -i input_cursor_line=5
+
+# 입력을 받는 커서의 열입니다.
 declare -i input_cursor_col=0
 
+# rm_helper의 옵션 메뉴얼입니다.
+rm_helper_manual=`cat resource/rm_helper_manual.txt`
+
+
+#######################################
+# 터미널에 rm_helper 메뉴얼을 표시합니다.
+# Globals:
+#   rm_helper_manual
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
+function draw_rm_helper_manual() {
+    echo "${rm_helper_manual}"
+}
+
+
+#######################################
+# rm_helper를 실행합니다.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   None
+#######################################
 function rm_helper() {
+    # 시작 전에 터미널을 초기화합니다.
     clear_terminal
 
-    rm_helper_manual=`cat resource/rm_helper_manual.txt`
-    echo "${rm_helper_manual}"
 
+    # rm_helper 메뉴얼을 터미널에 표시합니다. 
+    draw_rm_helper_manual
+
+
+    # rm 명령을 수행할 경로를 입력받습니다.
     move_cursor ${input_cursor_line} ${input_cursor_col}
     echo "rm 명령어를 수행할 파일 혹은 디렉토리 경로를 입력하세요..."
     read -p "> " rm_target_path
 
+
+    # rm 옵션을 입력 받습니다.
     move_cursor `expr ${input_cursor_line} + 2` ${input_cursor_col}
     echo "rm 명령어의 옵션을 -를 제외하고 입력하세요..."
     read -p "> " rm_target_option
 
+
+    # 사용자의 입력에 맞게 rm 명령을 수행합니다.
     if [ "${rm_target_path}" != "" ]; then
         `rm -${rm_target_option} ${rm_target_path}`
     fi
 }
 
+# rm_helpe를 실행합니다.
 rm_helper
