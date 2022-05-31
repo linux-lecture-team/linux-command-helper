d5ab9ef93fd8317ad41d3c8329d0ad9a0d4a7e7e
DESKTOP-4QAGCGF\choij <choijione02@gmail.com>
2022-05-31 22:57:59 +0900

    [2019203087] ls_helper 동작 부분에 주석문 추가

--- source/ls_helper.sh
+++ source/ls_helper.sh
@@ -41,21 +41,28 @@ function ls_helper() {
     # ls_helper 메뉴얼을 터미널에 표시합니다.
     draw_ls_helper_manual
     
-    
+
+    # ls 명령을 수행할 경로를 입력받습니다.
     move_cursor ${input_cursor_line} ${input_cursor_col}
     echo "ls 명령어를 수행할 절대 경로 혹은 상대 경로를 입력하세요..."
     read -p "> " ls_target_path
 
+
+    # ls 옵션을 입력받습니다.
     move_cursor `expr ${input_cursor_line} + 2` ${input_cursor_col}
     echo "ls 명령어의 옵션을 -를 제외하고 입력하세요..."
     read -p "> " ls_target_option
 
+
+    # 사용자의 입력에 맞게 ls 명령을 수행합니다.
     if [ "${ls_target_option}" == "" ]; then
         ls_execute_result=`ls ${ls_target_path}`
     else
         ls_execute_result=`ls -${ls_target_option} ${ls_target_path}`
     fi
     
+
+    # ls 명령 결과를 출력합니다.
     echo "${ls_execute_result}"
 }
 
